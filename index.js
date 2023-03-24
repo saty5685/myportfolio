08e40eaa56d8b631133d8712bca3e651db8416f0
Satyam  Singh <satyam@turings.xyz>
2023-03-25 00:47:34 +0530

    personal info added

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const express=require('express')
 const app=express()
 const path = require('path');
 const messageRouter=require('./routes/messageRoute')
-const mongoose=require("mongoose");
-const bodyParser=require("body-parser");
-mongoose.connect(process.env.DATABASE_URL)
-const db=mongoose.connection
-db.once('open',()=>console.log("connected "))
-db.on('error1',(error)=>console.error(error))
+// const mongoose=require("mongoose");
+// const bodyParser=require("body-parser");
+// mongoose.connect(process.env.DATABASE_URL)
+// const db=mongoose.connection
+// db.once('open',()=>console.log("connected "))
+// db.on('error1',(error)=>console.error(error))
 //register view engine
 app.set('view engine','ejs')
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended:true}));
+// app.use(bodyParser.urlencoded({extended:true}));
 //app.use(express.static(__dirname + '/public'));
 app.get('/',(req,res)=>{
     res.render('home')
